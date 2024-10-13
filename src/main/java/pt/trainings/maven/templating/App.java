b4f1d859171b757d58695806201c068c941155c9
TaZofia <167978200+TaZofia@users.noreply.github.com>
2024-10-13 15:52:11 +0200

    Update App.java

--- src/main/java/pt/trainings/maven/templating/App.java
+++ src/main/java/pt/trainings/maven/templating/App.java
@@ -4,7 +4,7 @@ package pt.trainings.maven.templating;
  * Hello world!
  */
 public class App {
-    //wersja z intellij
+    //wersja ze strony
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
