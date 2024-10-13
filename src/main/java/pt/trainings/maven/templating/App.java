347a9472095dccc8c0bda1dde9912b454f109a17
zofia <ztkal2013@gmail.com>
2024-10-13 15:50:59 +0200

    ...

--- src/main/java/pt/trainings/maven/templating/App.java
+++ src/main/java/pt/trainings/maven/templating/App.java
@@ -4,6 +4,7 @@ package pt.trainings.maven.templating;
  * Hello world!
  */
 public class App {
+    //wersja z intellij
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
