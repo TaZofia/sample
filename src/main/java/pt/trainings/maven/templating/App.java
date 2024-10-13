ef6bef532e05e2a3c96aa74b96cff0a0f80b0fea
TaZofia <167978200+TaZofia@users.noreply.github.com>
2024-10-13 15:43:35 +0200

    Update App.java

--- src/main/java/pt/trainings/maven/templating/App.java
+++ src/main/java/pt/trainings/maven/templating/App.java
@@ -4,6 +4,7 @@ package pt.trainings.maven.templating;
  * Hello world!
  */
 public class App {
+    //wersja ze strony konflikt 
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
